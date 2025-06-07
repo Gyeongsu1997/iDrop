82c307a5d0dac1e1745dec25b5107236461be446
gyeongsu <choiiisu11@gmail.com>
2025-06-07 17:39:03 +0900

    refactor: 사용되지 않는 generateMarker 함수 제거
    * 해당 함수 대신 hooks/useMarker.js에 정의된 useMarker 함수가 사용되는 것으로 확인함

--- FE/src/utils/map.js
+++ FE/src/utils/map.js
@@ -1,22 +1,3 @@
-const initMapOptions = {
-  mapDataControl: false,
-  scaleControl: true,
-  scaleControlOptions: {
-    position: naver.maps.Position.TOP_RIGHT,
-  },
-  logoControl: true,
-  logoControlOptions: {
-    position: naver.maps.Position.TOP_LEFT,
-  },
-};
-
-export function generateMarker(map, position) {
-  return new naver.maps.Marker({
-    position: position,
-    map: map,
-  });
-}
-
 export function getLatLng(x, y) {
   return new naver.maps.LatLng(x, y);
 }
